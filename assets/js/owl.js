ddab2977ddd7139cb2acc662852691ecbfbbf363
thanvanlong <longthan0409@gmail.com>
2023-03-23 13:31:40 +0700

    the first commit

---
+++ assets/js/owl.js
@@ -0,0 +1,35 @@
+$('.owl-carousel2').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    autoplay: true,
+    autoplayTimeout:3000,
+    autoplayHoverPause: true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+       
+    }
+})
+
+$('.owl-carousel1').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    autoplay: true,
+    autoplayTimeout:3000,
+    autoplayHoverPause: true,
+    responsive:{
+        0:{
+            items:1
+        }
+       
+    }
+})
